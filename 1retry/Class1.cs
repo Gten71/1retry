4fdc59aa85b1741be3eac45bf56ab2cb60166d2b
Gten <mresnyansky@gmail.com>
2022-06-12 10:47:01 +0200

    Добавьте файлы проекта.

---
+++ 1retry/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    internal class Class1
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public Class1(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double Raznost()
+        {
+            return Math.Sqrt(A - B /2 );
+        }
+    }
+}
